cadc1bbc2cbaead7c76f7bd4ca1c3c568a9536e7
Pedro Marques Freire <pedromarques1011@gmail.com>
2022-10-03 14:49:45 -0300

    feat: user service tests

--- Tryitter.Test/Repository/TryitterTestContext.cs
+++ Tryitter.Test/Repository/TryitterTestContext.cs
@@ -18,7 +18,25 @@ public class TryitterTestContext : DatabaseContext
 
         //usamos a função UseInMemoryDatabase() para indicar que usaremos um banco de dados utilizando a memória interna 
         optionsBuilder.UseInMemoryDatabase("User").UseInternalServiceProvider(serviceProvider);
-        optionsBuilder.UseInMemoryDatabase("Post").UseInternalServiceProvider(serviceProvider);
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Post>()
+          .HasKey(i => i.PostId);
+        modelBuilder.Entity<User>()
+          .HasKey(i => i.UserId);
+
+        // modelBuilder.Entity<Post>()
+        //   .HasOne(i => i.User)
+        //   .WithMany(i => i.Posts)
+        //   .HasForeignKey(b => b.UserId)
+        //   .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<User>()
+          .HasMany(i => i.Posts)
+          .WithOne(i => i.User)
+          .HasForeignKey(b => b.PostId)
+          .OnDelete(DeleteBehavior.Cascade);
     }
 
 }
---
+++ Tryitter.Test/Repository/UserRepositoryTest.cs
@@ -0,0 +1,57 @@
+using Tryitter.Web.Models;
+using Tryitter.Test.Repository;
+using Tryitter.Web.Services;
+using Tryitter.Web.Repository;
+using System;
+using System.Threading.Tasks;
+
+namespace Tryitter.Test.Service
+{
+    public class UserRepositoryTest
+    {
+        [Theory]
+        [InlineData("Joao", "joao123", "1234")]
+        [InlineData("Albert", "1234", "senha123")]
+        [InlineData("a", "a123", "1010")]
+        public async void ShouldCreateAnUser(string nickname, string login, string password)
+        {
+            //Arrange
+            TryitterTestContext tryitterTestContext = new();
+            UserRepository userRepository = new(tryitterTestContext);
+
+            User user = new() { Nickname = nickname, Login = login, Password = password };
+
+            //Act
+            var result = await userRepository.Add(user);
+            var userSaved = await userRepository.Get(result.UserId);
+
+            //Assert
+            result.Should().Be(user);
+            userSaved.Should().Be(user);
+
+        }
+
+        [Theory]
+        [InlineData("Joao", "joao123", "1234")]
+        [InlineData("Albert", "1234", "senha123")]
+        [InlineData("a", "a123", "1010")]
+        public async void ShouldThrownAnErrorWhenUserWithTheSameLoginAlreadyExists(string nickname, string login, string password)
+        {
+            //Arrange
+            User user = new() { Nickname = nickname, Login = login, Password = password };
+            var context = new TryitterTestContext();
+            var mockRepository = new Mock<IUserRepository>();
+
+            mockRepository.Setup(library => library.GetUserByLoginName(login)).ReturnsAsync(user); //forcar a funcao a retornar um valor assincrono
+            mockRepository.Setup(library => library.Add(user)).ReturnsAsync(user);
+
+            var service = new UserService(mockRepository.Object);
+
+            //Act
+            Func<Task> act = async () => await service.CreateUser(user);
+
+            //Assert
+            await act.Should().ThrowAsync<ArgumentException>();
+        }
+    }
+}
