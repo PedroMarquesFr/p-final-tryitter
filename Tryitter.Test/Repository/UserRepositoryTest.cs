9004942037c363400e48c92319755cae1368b7f3
Pedro Marques Freire <pedromarques1011@gmail.com>
2022-10-10 17:09:18 -0300

    feat: user repository unit tests

--- Tryitter.Test/Repository/UserRepositoryTest.cs
+++ Tryitter.Test/Repository/UserRepositoryTest.cs
@@ -47,11 +47,104 @@ namespace Tryitter.Test.Service
             var result = await userRepository.Add(user);
             var userSaved = await userRepository.Get(result.UserId);
             userSaved.Should().BeEquivalentTo(user);
-            await userRepository.Delete(result.UserId); 
+            await userRepository.Delete(result.UserId);
 
             //Assert
-            var userOnDatabase = await userRepository.GetAll();
-            // userOnDatabase.Count.Should().BeEmpty();
+            var userOnDatabase = await userRepository.Get(result.UserId);
+            userOnDatabase.Should().Be(null);
+
+        }
+
+        [Theory]
+        [InlineData("Joao", "joao123", "1234")]
+        [InlineData("Albert", "1234", "senha123")]
+        [InlineData("a", "a123", "1010")]
+        public async void ShouldUpdateAnUser(string nickname, string login, string password)
+        {
+            //Arrange
+            var novoPassword = "novoPassword";
+            TryitterTestContext tryitterTestContext = new();
+            UserRepository userRepository = new(tryitterTestContext);
+
+            User user = new() { Nickname = nickname, Login = login, Password = password };
+
+            //Act
+            var result = await userRepository.Add(user);
+            var userSaved = await userRepository.Get(result.UserId);
+            userSaved.Should().BeEquivalentTo(user);
+            userSaved!.Password = novoPassword;
+            await userRepository.Update(userSaved);
+
+            //Assert
+            var userOnDatabase = await userRepository.Get(result.UserId);
+            userOnDatabase!.Password.Should().Be(novoPassword);
+
+        }
+
+        [Theory]
+        [InlineData("Joao", "joao123", "1234")]
+        [InlineData("Albert", "1234", "senha123")]
+        [InlineData("a", "a123", "1010")]
+        public async void ShouldGetAnUser(string nickname, string login, string password)
+        {
+            //Arrange
+            TryitterTestContext tryitterTestContext = new();
+            UserRepository userRepository = new(tryitterTestContext);
+
+            User user = new() { Nickname = nickname, Login = login, Password = password };
+
+            //Act
+            var result = await userRepository.Add(user);
+            var userSaved = await userRepository.Get(result.UserId);
+
+            //Assert
+            userSaved.Should().BeEquivalentTo(user);
+
+        }
+
+        [Theory]
+        [InlineData("Joao", "joao123", "1234")]
+        [InlineData("Albert", "1234", "senha123")]
+        [InlineData("a", "a123", "1010")]
+        public async void ShouldGetAllUsers(string nickname, string login, string password)
+        {
+            //Arrange
+            TryitterTestContext tryitterTestContext = new();
+            UserRepository userRepository = new(tryitterTestContext);
+
+            User user = new() { Nickname = nickname, Login = login, Password = password };
+            User user2 = new() { Nickname = nickname, Login = login, Password = password };
+            var users = new User[] { user, user2 };
+
+            //Act
+            await userRepository.Add(user);
+            await userRepository.Add(user2);
+            var userSaved = await userRepository.GetAll();
+
+            //Assert
+            userSaved.Should().BeEquivalentTo(users);
+
+        }
+
+
+        [Theory]
+        [InlineData("Joao", "joao123", "1234")]
+        [InlineData("Albert", "1234", "senha123")]
+        [InlineData("a", "a123", "1010")]
+        public async void ShouldGetUserByLoginName(string nickname, string login, string password)
+        {
+            //Arrange
+            TryitterTestContext tryitterTestContext = new();
+            UserRepository userRepository = new(tryitterTestContext);
+
+            User user = new() { Nickname = nickname, Login = login, Password = password };
+
+            //Act
+            var result = await userRepository.Add(user);
+            var userSaved = await userRepository.GetUserByLoginName(user.Login);
+
+            //Assert
+            result.Should().BeEquivalentTo(userSaved);
 
         }
     }
