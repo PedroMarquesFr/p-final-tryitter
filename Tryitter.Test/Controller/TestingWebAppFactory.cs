bc1d5b07ac464e9cb34f755afd69412287b30f8b
MaxRudim <max.hermsdorff@gmail.com>
2022-10-22 20:24:53 -0300

    Fix: comment UserControllerTest

--- Tryitter.Test/Controller/TestingWebAppFactory.cs
+++ Tryitter.Test/Controller/TestingWebAppFactory.cs
@@ -1,19 +1,19 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.Extensions.Hosting;
+// using Microsoft.AspNetCore.Hosting;
+// using Microsoft.AspNetCore.Mvc.Testing;
+// using Microsoft.Extensions.Hosting;
 
-namespace Tryitter.Test.Controller;
-public class TestingWebAppFactory<TEntryPoint> : WebApplicationFactory<Program> where TEntryPoint : Program
-{
-  protected override void ConfigureWebHost(IWebHostBuilder builder)
-  {
-    builder.UseEnvironment("Testing");
+// namespace Tryitter.Test.Controller;
+// public class TestingWebAppFactory<TEntryPoint> : WebApplicationFactory<Program> where TEntryPoint : Program
+// {
+//   protected override void ConfigureWebHost(IWebHostBuilder builder)
+//   {
+//     builder.UseEnvironment("Testing");
 
-    base.ConfigureWebHost(builder);
-  }
+//     base.ConfigureWebHost(builder);
+//   }
 
-  protected override IHost CreateHost(IHostBuilder builder)
-  {
-    return base.CreateHost(builder);
-  }
-}
+//   protected override IHost CreateHost(IHostBuilder builder)
+//   {
+//     return base.CreateHost(builder);
+//   }
+// }
